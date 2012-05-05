ebc972f2cacb3a68cc2a65ebd5688abac9296955
phi <phi1618jp@gmail.com>
2012-05-05 22:31:43 +0900

    tmlib.js サンプル追加

---
+++ draft/tmlib-js-graphics-dotou.md
@@ -0,0 +1,5 @@
+# tmlib.js graphics 怒涛の 120 tips
+
+
+## 基本
+- [Canvas インスタンスを生成しよう](../samples/tmlib.js/graphics/sample.html)
---
+++ samples/tmlib.js/graphics/base/010.js
@@ -0,0 +1,9 @@
+/**
+ * canvas の幅を指定しよう
+ */
+
+// canvas インスタンス生成
+var canvas = tm.graphics.Canvas("#world");
+
+// 幅を指定
+canvas.width = 465;
