59672fecd2ebe4aaf36228d408798e2d8d605b54
phi <phi1618jp@gmail.com>
2012-07-31 13:19:56 +0900

    fix.

--- samples/daily-countdown/main.js
+++ samples/daily-countdown/main.js
@@ -1,28 +1,39 @@
 /*
- * main
+ * const
  */
-tm.main(function() {
-    var eTime = tm.dom.Element("#main .time");
-    eTime.html = getDailyLimit();
-    
-    tm.setLoop(function() {
-        eTime.html = getDailyLimit();
-        if (eTime.html == 0) {
-            alert("Congratulations see you tomorrow.");
-        }
-    }, 1000)
-});
-
+var DAY = 60*60*60;
 
 /*
  * 一日の残り時間を取得
  */
 var getDailyLimit = function() {
-    var now = new Date();
-    var hours = now.getHours() * 3600;
-    var minutes = now.getMinutes() * 60;
-    var seconds = now.getSeconds();
-    var limit = 86400 - (hours+minutes+seconds);
+    var now     = new Date();
     
-    return limit;
+    var hours   = now.getHours() * 3600;        // 時→秒
+    var minutes = now.getMinutes() * 60;        // 分→秒
+    var seconds = now.getSeconds();　                            // 秒
+    var elapsed_time = (hours+minutes+seconds); // 経過時間
+    var limit_time   = DAY - elapsed_time;      // 明日までのリミット
+    
+    return limit_time;
 };
+
+
+/*
+ * main
+ */
+tm.main(function() {
+    // 表示更新
+    var eTime = tm.dom.Element("#main .time");  // 要素取得
+    eTime.html = getDailyLimit();               // 残り時間をセット
+    
+    // ループ処理をセット
+    tm.setLoop(function() {
+        // 表示更新
+        eTime.html = getDailyLimit();
+        // チェック
+        if (eTime.html == 0) {
+            alert("Congratulations see you tomorrow.");
+        }
+    }, 1000)
+});
